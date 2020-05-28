26c53252658aa2fb295e1268135d5b29f579abe0
unknown <pablolasota2@gmail.com>
2020-05-28 00:35:20 -0300

    continuar... explicacion del ejercicio de la clase 3

---
+++ Win/clase-3-ejercicio-1(ConExplicacion)/src/clase-3-ejercicio-1(ConExplicacion).c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        : clase-3-ejercicio-1(ConExplicacion).c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+
+/*EJERCICIO
+ * Realizar un programa que pida un numero al usuario
+ * que pida una opcion (+, -, *, /)
+ * pedir otro numero para operar
+ * ejecute una la funcion corespondiendte (sumaDosEnteros(), restaDosEnteros(), multiplicaDosEnteros(), divideDosEnteros())
+ * mostrar el resultado al usuario
+ *
+ * NOTA: recordar que el resultado de la division puede ser un flotante
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+}
